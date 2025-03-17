f95e27eafdc750fc2f89dd0723c9d8e20bd7751d
MaximoFC <maximofcallejas@gmail.com>
2025-03-17 14:13:27 -0300

    Checking Accounts controller created

---
+++ src/controllers/checkingaccount.controller.js
@@ -0,0 +1,24 @@
+const CheckingAccount = require('../models/CheckingAccount');
+
+const createCheckingAccount = async (req, res) => {
+    const { ARS, USD, EUR, id_client } = req.body;
+    try {
+        const newCheckingAccount = await CheckingAccount.createCheckingAccount(ARS, USD, EUR, id_client);
+        res.status(201).json(newCheckingAccount);
+    } catch (error) {
+        console.error('Error creating checking account: ', error.message);
+        res.status(500).json({error: 'Error creating checking account.'});
+    }
+};
+
+const getCheckingAccount = async (req, res) => {
+    try {
+        const checkingAccounts = await CheckingAccount.getCheckingAccounts();
+        res.status(200).json(checkingAccounts);
+    } catch (error) {
+        console.error('Error fetching checking accounts: ', error.message);
+        res.status(500).json({error: 'Error fetching checking accounts.'});
+    }
+}
+
+module.exports = { createCheckingAccount, getCheckingAccount }
